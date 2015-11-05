bd38957bfe6f5e8640a4e8cdfcd88ff94b67f1b0
Henrique Barbieri <henriquetb@gmail.com>
2015-11-05 12:08:06 +1100

    Refactored the class and added a getLoggedUser function.

--- src/main/resources/static/js/stores/UserStore.js
+++ src/main/resources/static/js/stores/UserStore.js
@@ -6,22 +6,23 @@ var assign = require('object-assign');
 
 var CHANGE_EVENT = 'change';
 
-var _todos = {};
+var _users = {};
+
 
 /**
  * Create a TODO item.
  * @param  {string} text The content of the TODO
  */
 function create(text) {
-  // Hand waving here -- not showing how this interacts with XHR or persistent
-  // server-side storage.
-  // Using the current timestamp + random number in place of a real id.
-  var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
-  _todos[id] = {
-    id: id,
-    complete: false,
-    text: text
-  };
+	// Hand waving here -- not showing how this interacts with XHR or persistent
+	// server-side storage.
+	// Using the current timestamp + random number in place of a real id.
+	var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
+	_users[id] = {
+			id: id,
+			complete: false,
+			text: text
+	};
 }
 
 /**
@@ -31,66 +32,65 @@ function create(text) {
  *     updated.
  */
 function update(id, updates) {
-  _todos[id] = assign({}, _todos[id], updates);
+	_users[id] = assign({}, _users[id], updates);
 }
 
 var UserStore = assign({}, EventEmitter.prototype, {
 
-  /**
-   * Get the entire collection of TODOs.
-   * @return {object}
-   */
-  getAll: function() {
-    return _todos;
-  },
+	getLoggedUser: function() {
+		return {
+			id: 1,
+			name: "Henrique"
+		}
+	},
 
-  emitChange: function() {
-    this.emit(CHANGE_EVENT);
-  },
+	emitChange: function() {
+		this.emit(CHANGE_EVENT);
+	},
 
-  /**
-   * @param {function} callback
-   */
-  addChangeListener: function(callback) {
-    this.on(CHANGE_EVENT, callback);
-  },
+	/**
+	 * @param {function} callback
+	 */
+	addChangeListener: function(callback) {
+		this.on(CHANGE_EVENT, callback);
+	},
 
-  /**
-   * @param {function} callback
-   */
-  removeChangeListener: function(callback) {
-    this.removeListener(CHANGE_EVENT, callback);
-  }
+	/**
+	 * @param {function} callback
+	 */
+	removeChangeListener: function(callback) {
+		this.removeListener(CHANGE_EVENT, callback);
+	}
 });
 
-// Register callback to handle all updates
+//Register callback to handle all updates
 AppDispatcher.register(function(action) {
-  var text;
+	var text;
 
-  switch(action.actionType) {
-    case UserConstants.USER_CREATE:
-      text = action.text.trim();
-      if (text !== '') {
-        create(text);
-        UserStore.emitChange();
-      }
-      break;
+	switch(action.actionType) {
+	case UserConstants.USER_CREATE:
+		text = action.text.trim();
+		if (text !== '') {
+			create(text);
+			UserStore.emitChange();
+		}
+		break;
 
-    case UserConstants.USER_EDIT:
-      text = action.text.trim();
-      if (text !== '') {
-        update(action.id, {text: text});
-        UserStore.emitChange();
-      }
-      break;
+	case UserConstants.USER_EDIT:
+		text = action.text.trim();
+		if (text !== '') {
+			update(action.id, {text: text});
+			UserStore.emitChange();
+		}
+		break;
 
-    case UserConstants.RECEIVE_RAW_USERS:
-    	console.log("received");
-      break;
-      
-    default:
-      // no op
-  }
+	case UserConstants.RECEIVE_RAW_USERS:
+		console.log("received");
+		break;
+
+	default:
+		// no op
+	}
 });
 
 module.exports = UserStore;
